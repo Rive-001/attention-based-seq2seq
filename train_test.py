2fcc57016037a191e48233264e5d00e4913862c9
Rishi Verma <rishioffic@gmail.com>
2021-04-11 14:13:13 -0400

    Update comments

--- train_test.py
+++ train_test.py
@@ -5,7 +5,6 @@ from util import plot_attn_flow
 import torch.nn as nn
 LETTER_LIST = ['<pad>', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', \
                'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '-', "'", '.', '_', '+', ' ','<sos>','<eos>']
-### Add Your Other Necessary Imports Here! ###
 
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 
@@ -101,35 +100,6 @@ def train(model, train_loader, criterion, optimizer, scheduler, epoch, exp=0):
 
         file.write('Train Perplexity:{}\n'.format(perplexity))
 
-
-
-    # 1) Iterate through your loader
-        # 2) Use torch.autograd.set_detect_anomaly(True) to get notices about gradient explosion
-        
-            # 3) Set the inputs to the device.
-
-            # 4) Pass your inputs, and length of speech into the model.
-
-            # 5) Generate a mask based on the lengths of the text to create a masked loss. 
-            # 5.1) Ensure the mask is on the device and is the correct shape.
-
-            # 6) If necessary, reshape your predictions and origianl text input 
-            # 6.1) Use .contiguous() if you need to. 
-
-            # 7) Use the criterion to get the loss.
-
-            # 8) Use the mask to calculate a masked loss. 
-
-            # 9) Run the backward pass on the masked loss. 
-
-            # 10) Use torch.nn.utils.clip_grad_norm(model.parameters(), 2)
-            
-            # 11) Take a step with your optimizer
-
-            # 12) Normalize the masked loss
-
-            # 13) Optionally print the training loss after every N batches
-
     end = time.time()
     # print('Train time:',end-start,'seconds')
 
@@ -195,7 +165,6 @@ def val(model, val_loader, epoch, exp):
 
     
 def test(model, test_loader, exp):
-    ### Write your test code here! ###
     
     # model.load_state_dict(torch.load('BestModel{}.pth'.format(exp)))
     model.eval()
